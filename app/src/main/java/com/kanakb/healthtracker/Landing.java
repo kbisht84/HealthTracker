8c2cde16ebb6feab1a02c088d8f3cbf4657bbc9e
kbisht84 <kanakbisht9@gmail.com>
2015-10-23 15:52:41 -0700

    Created landing page with personal info for calorie count

--- app/src/main/java/com/kanakb/healthtracker/Landing.java
+++ app/src/main/java/com/kanakb/healthtracker/Landing.java
@@ -1,29 +1,78 @@
 package com.kanakb.healthtracker;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
-public class Landing extends AppCompatActivity {
+import de.congrace.exp4j.Calculable;
+import de.congrace.exp4j.ExpressionBuilder;
 
+public class Landing extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String gender=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public Double calorieCounter(double weight,double height,int age){
+            Double  result=null;
+        try {
+
+            if ("male".equals(gender)){
+                Calculable calc = new ExpressionBuilder("10 * "+weight + "+ 6.25 *"+ height+" - 5 * "+age +"+5").build();
+                 result = calc.calculate();
+            }
+
+            if("female".equals(gender)){
+                Calculable calc = new ExpressionBuilder("10 * "+weight + "+6.25 *"+ height+" - 5 * "+age+"-161").build();
+                 result = calc.calculate();
+
+            }
+
+
+
+
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        return result;
+    }
+
+
+
 
 }
