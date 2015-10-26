e1f2c402de6f1363e86803380b30c119ed8e4ad1
kbisht84 <kanakbisht9@gmail.com>
2015-10-26 16:05:50 -0700

    edit landing page

--- app/src/main/java/com/kanakb/healthtracker/Landing.java
+++ app/src/main/java/com/kanakb/healthtracker/Landing.java
@@ -3,6 +3,7 @@ package com.kanakb.healthtracker;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,18 +13,42 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.congrace.exp4j.Calculable;
 import de.congrace.exp4j.ExpressionBuilder;
 
 public class Landing extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private Button bmiButton;
+    private EditText height;
+    private EditText weight;
+    private EditText age;
+    private Spinner gender;
+    private TextView calculatedBmi;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        bmiButton=(Button)findViewById(R.id.button5);
+        height=(EditText)findViewById(R.id.editText8);
+        weight=(EditText)findViewById(R.id.editText9);
+        age=(EditText)findViewById(R.id.editText10);
+        gender=(Spinner)findViewById(R.id.spinner);
+        calculatedBmi=(TextView)findViewById(R.id.textView10);
+
+
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
@@ -34,6 +59,17 @@ public class Landing extends AppCompatActivity implements AdapterView.OnItemSele
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+
+
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Double bmi=calorieCounter(Double.parseDouble(weight.getText().toString())*0.45,Double.parseDouble(height.getText().toString())*30.48,Integer.parseInt(age.getText().toString()),gender.getSelectedItem().toString());
+                calculatedBmi.setText(bmi.toString());
+            }
+        });
 
     }
 
@@ -49,12 +85,12 @@ public class Landing extends AppCompatActivity implements AdapterView.OnItemSele
             Double  result=null;
         try {
 
-            if ("male".equals(gender)){
+            if ("Male".equals(gender)){
                 Calculable calc = new ExpressionBuilder("10 * "+weight + "+ 6.25 *"+ height+" - 5 * "+age +"+5").build();
                  result = calc.calculate();
             }
 
-            if("female".equals(gender)){
+            if("Female".equals(gender)){
                 Calculable calc = new ExpressionBuilder("10 * "+weight + "+6.25 *"+ height+" - 5 * "+age+"-161").build();
                  result = calc.calculate();
 
